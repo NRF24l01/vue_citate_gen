2e6e8a7e17d3047e569ed47a0cb1f04a591c2fe6
stenli <s.nevrovsky@ya.ru>
2025-03-26 11:25:24 +0300

    added clients

---
+++ src/components/OurClients.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col items-center space-y-6 py-12">
+      <h2 class="text-2xl font-bold">Наши клиенты</h2>
+      <div class="flex items-end space-x-6">
+        <div class="flex flex-col items-center w-1/4">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAh3-JQpkywnwYRGyEn06euflnYYZIt5wN8w&s" alt="Client 1" class="w-48 h-64 object-cover rounded-lg shadow-lg" />
+          <p class="mt-4 text-lg font-semibold">Дуэно Джносон</p>
+          <blockquote class="text-center text-gray-600 italic">“Отличный сервис, рекомендую!”</blockquote>
+        </div>
+        <div class="flex flex-col items-center w-1/3">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF1ZL7MEGa4Rr1rmlS64r5tmF01fxzx5nang&s" alt="Client 2" class="w-64 h-80 object-cover rounded-lg shadow-xl" />
+          <p class="mt-4 text-xl font-bold">Юра Бобрисов</p>
+          <blockquote class="text-center text-gray-700 italic">“Вот он точно заслужил оскара.”</blockquote>
+        </div>
+        <div class="flex flex-col items-center w-1/4">
+          <img src="https://sun1-84.userapi.com/s/v1/ig2/TwHAM7LLdBtM25xUj0oMKOcgJvRIq4Y_c8BrrZoZWL3yd8Mex3QFJi1Ff1dXRxayt5Ub4Rb9jCxxQMzJ8aopVqMS.jpg?quality=95&as=32x32,48x48,72x72,108x109,160x161,240x241,360x362,480x483,540x543,640x643,720x724,746x750&from=bu&u=dPXET3bTzQCBjfUz_jPM_1-H75BmfAyXbS4lx7_ZPV0&cs=746x750" alt="Client 3" class="w-48 h-64 object-cover rounded-lg shadow-lg" />
+          <p class="mt-4 text-lg font-semibold">Джейсон Стэтхем</p>
+          <blockquote class="text-center text-gray-600 italic">“Этот сервис удобный, как мой удар с разворота – быстро, чётко, без лишних движений.”</blockquote>
+        </div>
+      </div>
+    </div>
+  </template>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HelloHeader from '@/components/HelloHeader.vue';
+import OurClients from '@/components/OurClients.vue';
 </script>
 
 <template>
   <main>
     <HelloHeader />
+    <OurClients />
   </main>
 </template>
